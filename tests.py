38137c109590a5a1eb3acfb9451760a60bd15f6d
Enrico B. Palma <enricob.palma@gmail.com>
2021-09-17 16:46:31 -0300

    Tests Refactoring

--- tests.py
+++ tests.py
@@ -1,3 +1,18 @@
 #code
 import random
 from sorting import bubble_sort, merge_sort
+
+any_numbers = random.sample(range(1, 100000), 100000)
+
+if __name__ == '__main__':
+    test_cases = {'Random numbers': any_numbers}
+
+    print('>>>---<<< >>>---<<< >>>---<<< >>>---<<< >>>---<<<')
+
+    for name, lista in test_cases.items():
+        print('\nCaso de teste: {}'.format(name))
+        print(lista)
+        quicksort(lista)
+        print('\nOrdenado:')
+        print(lista)
+    print('>>>---<<< >>>---<<< >>>---<<< >>>---<<< >>>---<<<')
